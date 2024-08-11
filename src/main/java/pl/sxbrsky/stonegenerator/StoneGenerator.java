e022fd7cd45d49a301427d37bf7f2bed50a9a13b
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-08-11 09:40:55 +0200

    feat: initialize empty spigot plugin

---
+++ src/main/java/pl/sxbrsky/stonegenerator/StoneGenerator.java
@@ -0,0 +1,17 @@
+package pl.sxbrsky.stonegenerator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StoneGenerator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
